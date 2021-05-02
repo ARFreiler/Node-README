09bf2d6dc1f17c24792df34cfc70a62d86b75f6c
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-05-02 14:40:55 -0400

    File editing of readme, removed unused input field, added TEST field, and formatted email response.

--- index.js
+++ index.js
@@ -3,26 +3,18 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Inquirer package to generate questions to user.
-
 inquirer.prompt(
     [
         {
             type: 'input',
             message: 'What is the projects title?',
             name: 'Title',
-            // Validate the property to confirm the user has provided a value.
             validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
         },
         {
             type: 'input',
             message: 'How would you describe your app?',
             name: 'Description',
-            validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
-        },
-        {
-            type: 'input',
-            message: 'What is contained in your app?',
-            name: 'TableofContents',
             validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
         },
         {
@@ -38,10 +30,10 @@ inquirer.prompt(
             validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
         },
         {
-            type: 'input',
-            message: 'What license does your app use?',
+            type: 'list',
+            message: 'What license does your app use? Please choose one of the following: ',
             name: 'License',
-            choices: ['The MIT License', 'The GPL License', 'Apache License', 'GNU License', 'N/A'],
+            choices: ['The MIT License', 'GNU GPL v3', 'Apache 2.0', 'N/A'],
             validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
         },
         {
@@ -75,13 +67,24 @@ inquirer.prompt(
             validate: (value) => { if (value) { return true } else { return 'I need a value to continue.' } },
         }
     ]
-
+    // Template to display the README.md being created based on user input
 ).then((
     response
 ) => {
+    console.log(response);
+    // Generation of the License
+    let badge;
+    if (response.License === 'The MIT License') {
+        badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (response.License === 'GNU GPL v3') {
+        badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (response.License === 'Apache 2.0') {
+        badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else { badge = '' };
     const template = `# ${response.Title}
+${badge}
 ## Description
-${response.Description}    
+    ${response.Description}    
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
@@ -89,24 +92,26 @@ ${response.Description}
 * [Credits](#credits)
 * [License](#license)
 ## Installation
-${response.Installation}
+    ${response.Installation}
 ## Usage
-${response.Usage}
+    ${response.Usage}
 ## Contributors
-${response.Contributors}
+    ${response.Contributors}
 ## Credits
-${response.Credits}
+    ${response.Credits}
 ## License
-${response.License}
+    ${response.License}
+## Tests
+    ${response.Tests}
         
 ## Questions
-GitHub: ${response.GitHub}\r\n
-Email: ${response.Email}`;
+GitHub: [${response.GitHub}](https://github.com/${response.GitHub}) \r\n
+Email: [${response.Email}](https://${response.Email}) `;
     createNewFile(response.Title, template);
 });
-
+// Function generating the README.md
 function createNewFile(fileName, template) {
-    fs.writeFile(`./${fileName}.md`, template, (err) => {
+    fs.writeFile(`./ ${fileName}.md`, template, (err) => {
         if (err) {
             console.log(err)
         }
